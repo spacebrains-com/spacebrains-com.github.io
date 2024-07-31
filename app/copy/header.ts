5fabc1fc658642b0dec209e9ebdf236de433a6b7
ehsan <Ehsan.Saradar@gmail.com>
2024-07-31 13:23:51 +0330

    Fix menu item

--- app/copy/header.ts
+++ app/copy/header.ts
@@ -1,6 +1,6 @@
 export const headerCopy = {
     nav: [
-        { name: "Introduction", url: "#introduction", target: "_self" },
+        { name: "Introduction", url: "#Introduction", target: "_self" },
         { name: "Human Potential", url: "#human-potential", target: "_self" },
         { name: "Environmental Impact", url: "#environmental-impact", target: "_self" },
         { name: "Advanced Healthcare", url: "#advanced-healthcare", target: "_self" },
